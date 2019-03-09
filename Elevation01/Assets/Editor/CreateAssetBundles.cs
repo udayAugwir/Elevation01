26642df55107fb221f167f68031f20f19a4b54fa
uday <uday.bojja@augwir.com>
2019-03-09 18:19:33 +0530

    AssetBundle Cretion and Loading

---
+++ Elevation01/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreateAssetBundles : Editor
+{
+    [MenuItem("Assets/Create Asset Bundle")]
+    static void ExportBundle()
+    {
+        string bundlePath = "Assets/AssetBundle/GroundFloor.unity3d";
+        Object[] selectedAssets = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
+        BuildPipeline.BuildAssetBundle(Selection.activeObject, selectedAssets, bundlePath, BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets, BuildTarget.StandaloneOSX);
+
+    }
+
+}
---
+++ Elevation01/Assets/Elevation/Scripts/AssetDownloader.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AssetDownloader : MonoBehaviour
+{
+    public string url;
+    GameObject waterCraftGO;
+    public Text loadingText;
+    public Transform spawnPos;
+
+    IEnumerator LoadBundle(string GroundFloor)
+    {
+        while (!Caching.ready)
+        {
+            yield return null;
+        }
+
+        //Begin download
+        WWW www = WWW.LoadFromCacheOrDownload(url, 0);
+        yield return www;
+
+        //Load the downloaded bundle
+        AssetBundle bundle = www.assetBundle;
+
+        //Load an asset from the loaded bundle
+        AssetBundleRequest bundleRequest = bundle.LoadAssetAsync(GroundFloor, typeof(GameObject));
+        yield return bundleRequest;
+
+        //get object
+        GameObject obj = bundleRequest.asset as GameObject;
+
+        waterCraftGO = Instantiate(obj, spawnPos.position, Quaternion.identity) as GameObject;
+        loadingText.text = "";
+
+        bundle.Unload(false);
+        www.Dispose();
+    }
+
+    public void Load(string GroundFloor)
+    {
+        if (waterCraftGO)
+        {
+            Destroy(waterCraftGO);
+        }
+
+        loadingText.text = "Loading...";
+        StartCoroutine(LoadBundle(GroundFloor));
+    }
+}
